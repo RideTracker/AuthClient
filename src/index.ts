540cf3c5c918e76b2856a5d4a578769dd651089f
Nora Söderlund <norasoderlund@icloud.com>
2023-07-20 19:07:18 +0200

    Fixed exports

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * from "./controllers/clients/createAuthClient";
 export * from "./models/AuthClient";
 export * from "./models/AuthClientToken";
 export * from "./models/RequestMethod";
+export * from "./models/Fetcher";
